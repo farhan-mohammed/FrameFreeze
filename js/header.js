99835225bbea7466b4af5d467bf50d49f21bb9ec
Farhan-on-Desktop <farhanhm12@gmail.com>
2019-08-26 23:16:29 -0400

    Remodeled plus finished header and section mechanism

---
+++ js/header.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', () => {
+	const logoImage = document.querySelector('.logo img');
+	const mainNav = document.getElementById('#mainNav');
+	console.log();
+	if (window.pageYOffset > 0.3 * screen.height) {
+		logoImage.style.height = '7vh';
+	} else {
+		logoImage.style.height = '10vh';
+	}
+});
