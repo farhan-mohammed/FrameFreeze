285bff163b2671f5628ca99fb0fab7d38c43d403
Farhan-on-Desktop <farhanhm12@gmail.com>
2019-08-29 15:16:34 -0400

    Updated colors

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', () => {
 	const logoImage = document.querySelector('.logo img');
 	const mainNav = document.getElementById('#mainNav');
 	console.log();
-	if (window.pageYOffset > 0.3 * screen.height) {
+	if (window.pageYOffset > 0.15 * screen.height) {
 		logoImage.style.height = '7vh';
 	} else {
 		logoImage.style.height = '10vh';
---
+++ js/section5.js
@@ -0,0 +1,51 @@
+sectionFiveHeight = 0;
+changeHeight = 0;
+SetVariables = () => {
+	sections = document.getElementsByClassName('section');
+	this.sectionFiveHeight = sections[4].offsetHeight;
+	sectionHeight = Array.prototype.map.call(sections, (x) => x.offsetHeight).slice(0, 4);
+	this.changeHeight = sectionHeight.reduce((sum, current) => sum + current);
+};
+SetVariables();
+
+window.addEventListener('scroll', () => {
+	// if (window.scrollY >= this.changeHeight && light == false) {
+	// 	console.clear();
+	// 	console.log('WE ARE PAST');
+	// 	setColor(5);
+	// 	setColor(6);
+	// 	setColor(4);
+	// 	light = true;
+	// } else if (window.scrollY + window.innerHeight < this.changeHeight && light != false) {
+	// 	console.clear();
+	// 	console.log('NOT YET');
+	// 	removeColor(5);
+	// 	removeColor(6);
+	// 	removeColor(4);
+	// 	light = false;
+	// }
+	if (window.scrollY < this.changeHeight) {
+		document.getElementsByClassName(`sec-5`)[0].style.backgroundColor = 'rgba(7, 38, 84, 0)';
+	} else {
+		percentage = (window.scrollY - this.changeHeight) / this.sectionFiveHeight;
+		console.log(percentage);
+		/*
+            if the scroll bar is between 0 and 10% of the div
+                then the gradient will get softer to reveal the true color
+            if its past
+                set to true color
+        */
+		document.getElementsByClassName(`sec-5`)[0].style.backgroundColor = `rgba(7, 38, 84, ${percentage < 0.1
+			? percentage * 10
+			: 1})`;
+	}
+});
+// function setColor(x) {
+// 	document.getElementsByClassName(`sec-${x}`)[0].style.color = '#fff';
+// 	document.getElementsByClassName(`sec-${x}`)[0].style.backgroundColor = '#072654';
+// }
+// function removeColor(x) {
+// 	document.getElementsByClassName(`sec-${x}`)[0].style.color = '';
+// 	document.getElementsByClassName(`sec-${x}`)[0].style.backgroundColor = '';
+// }
+window.addEventListener('resize', SetVariables);
