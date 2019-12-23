d551f0aaafa3ecf394a61e29854f2d685e29cff2
Farhan Mohammed <farhanhm12@gmail.com>
2019-12-23 02:09:41 -0500

    Updates

---
+++ precache-manifest.aa3a40b1d234e38889c0b62bf2f6082c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39311d23886eec2127a28a905dde00d",
+    "url": "/FrameFreeze/index.html"
+  },
+  {
+    "revision": "41871f397fd0f2cd9a8b",
+    "url": "/FrameFreeze/static/css/main.d458fa13.chunk.css"
+  },
+  {
+    "revision": "2bef88961bcf61631f3f",
+    "url": "/FrameFreeze/static/js/2.7bcc2f93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FrameFreeze/static/js/2.7bcc2f93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41871f397fd0f2cd9a8b",
+    "url": "/FrameFreeze/static/js/main.6ed0b0db.chunk.js"
+  },
+  {
+    "revision": "5365fe8e4ff0388f03e7",
+    "url": "/FrameFreeze/static/js/runtime-main.ec005ad6.js"
+  },
+  {
+    "revision": "a7c776071533cae51aa2dc8044f091c9",
+    "url": "/FrameFreeze/static/media/cavernback.a7c77607.png"
+  },
+  {
+    "revision": "42e115c670821e59254bb9a626397936",
+    "url": "/FrameFreeze/static/media/cavernfront.42e115c6.png"
+  },
+  {
+    "revision": "ff98895ceb7719a65fe3b29311525c19",
+    "url": "/FrameFreeze/static/media/cavernmid.ff98895c.png"
+  },
+  {
+    "revision": "4c9ff01104411495353e2909bb5f6329",
+    "url": "/FrameFreeze/static/media/ffne.4c9ff011.png"
+  },
+  {
+    "revision": "a97a5443dc1019a233379ee00bb0c919",
+    "url": "/FrameFreeze/static/media/footer.a97a5443.png"
+  },
+  {
+    "revision": "fa1876d1e77acfe3aa6bb7fadc130bd4",
+    "url": "/FrameFreeze/static/media/layer-1.fa1876d1.png"
+  },
+  {
+    "revision": "ae91980e72968dd8b726b13170e312bb",
+    "url": "/FrameFreeze/static/media/layer-2.ae91980e.png"
+  },
+  {
+    "revision": "070e84eda898495e4ea1f31beca9411c",
+    "url": "/FrameFreeze/static/media/layer-3.070e84ed.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/farhan-mohammed/FrameFreeze/precache-manifest.8b1016653f7bc6311991b1feb1afc4ef.js"
+  "/FrameFreeze/precache-manifest.aa3a40b1d234e38889c0b62bf2f6082c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/farhan-mohammed/FrameFreeze/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FrameFreeze/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
